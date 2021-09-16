19dcab244d5317c5c8c116f2a8357eec910e9787
bilbe-tri <75400844+bilbe-tri@users.noreply.github.com>
2021-09-16 13:03:34 -0400

    Update Kid_account_accessStep.java

--- src/test/java/com/glue/Kid_account_accessStep.java
+++ src/test/java/com/glue/Kid_account_accessStep.java
@@ -26,7 +26,7 @@ public class Kid_account_accessStep {
         Assert.assertEquals(true, true);
     }
     
-    @Then("^something good should happen$") //modify a comment
+    @Then("^something good should happen$") //change this comment
     public void something_good_should_happen() throws Throwable {
         Assert.assertEquals(true, true);
     }
