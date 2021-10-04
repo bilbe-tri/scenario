7e1f7de166ca0f8dab945021dc0a9dbfa20ce93c
bilbe-tri <75400844+bilbe-tri@users.noreply.github.com>
2021-10-04 13:13:14 -0400

    Create WorkbenchCuttingPlatformSteps.java

---
+++ src/test/java/com/glue/WorkbenchCuttingPlatformSteps.java
@@ -0,0 +1,26 @@
+package com.glue;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class WorkbenchCuttingPlatformStep {
+
+    @Given("^I have a workbench without a cutting stations$")
+    public void i_have_a_workbench() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+
+  @When("^I want to cut things to length$")
+  public void i_need_to_cut_material() throws Throwable {
+      // Write code here that turns the phrase above into concrete actions
+      Assert.assertEquals(true, true);
+  }
+
+  @Then("^I should have a chop saw station$")
+  public void add_a_cutting_station() throws Throwable {
+      // Write code here that turns the phrase above into concrete actions
+      Assert.assertEquals(true, flase);
+  }
+}
