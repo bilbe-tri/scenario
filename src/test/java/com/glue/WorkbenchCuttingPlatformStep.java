a2ed3e3a3e60a46e9651adcbd15bd5b07d251d21
bilbe-tri <75400844+bilbe-tri@users.noreply.github.com>
2021-10-04 13:42:03 -0400

    Update WorkbenchCuttingPlatformStep.java

--- src/test/java/com/glue/WorkbenchCuttingPlatformStep.java
+++ src/test/java/com/glue/WorkbenchCuttingPlatformStep.java
@@ -8,19 +8,19 @@ import org.junit.Assert;
 public class WorkbenchCuttingPlatformStep {
 
     @Given("^I have a workbench without a cutting station$")
-    public void i_have_a_workbench() throws Throwable {
+    public void i_have_a_workbench_without_a_cutting_station() throws Throwable {
         Assert.assertEquals(true, true);
     }
 
   @When("^I want to cut things to length$")
-  public void i_need_to_cut_material() throws Throwable {
+  public void i_want_to_cut_things_to_length() throws Throwable {
       // Write code here that turns the phrase above into concrete actions
       Assert.assertEquals(true, true);
   }
 
   @Then("^I should have a chop saw station$")
-  public void add_a_cutting_station() throws Throwable {
+  public void i_should_have_a_chop_saw_station() throws Throwable {
       // Write code here that turns the phrase above into concrete actions
-      Assert.assertEquals(false, false);
+      Assert.assertEquals(true, false);
   }
 }
