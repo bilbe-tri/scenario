f4821b51ed10a79705b86af2d7496efab4697895
bilbe-tri <75400844+bilbe-tri@users.noreply.github.com>
2022-11-28 20:23:15 -0500

    Add files via upload
    Create .java from from snippets reported by Jenkins

---
+++ src/test/java/com/glue/CoffeeShopStep.java
@@ -0,0 +1,20 @@
+package com.glue;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class CoffeeShopStep {
+   @Given("^today is Monday$")
+   public void today_is_Monday() throws Throwable {
+       // Write code here that turns the phrase above into concrete actions
+       throw new PendingException();
+   }
+
+   @Then("^the shop is closed$")
+   public void the_shop_is_closed() throws Throwable {
+       // Write code here that turns the phrase above into concrete actions
+       throw new PendingException();
+   }
+}
